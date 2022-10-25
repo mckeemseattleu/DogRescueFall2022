115ad230d81b964302c69dc9f21986aaefeeb16e
mckeemseattleu <mckeem@seattleu.edu>
2022-10-25 14:43:20 -0700

    Added "Name" to our ProductModel model in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -14,6 +14,7 @@ namespace ContosoCrafts.WebSite.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+        public string Name { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
