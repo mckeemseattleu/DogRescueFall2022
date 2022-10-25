7c25023cabe837378a3cda6959a60e0025879bed
mckeemseattleu <mckeem@seattleu.edu>
2022-10-25 16:41:43 -0700

    Moved Name to an earlier location in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -7,13 +7,13 @@ namespace ContosoCrafts.WebSite.Models
     {
         public string Id { get; set; }
         public string Maker { get; set; }
-        
+        public string Name { get; set; }
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
-        public string Name { get; set; }
+        public string DogType { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
