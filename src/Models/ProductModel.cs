44e48adfea5ca240ffd0aa7a4e3cbdd9d40b2293
mckeemseattleu <mckeem@seattleu.edu>
2022-10-25 15:27:09 -0700

    Removed "Tittle" from Model in ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -11,7 +11,6 @@ namespace ContosoCrafts.WebSite.Models
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
-        public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
         public string Name { get; set; }
