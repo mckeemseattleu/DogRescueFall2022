26cb28ef3975b3f622e8a0ba559832282d89dc69
mckeemseattleu <mckeem@seattleu.edu>
2022-10-25 18:53:00 -0700

    Removed Ratings[] from ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -12,7 +12,6 @@ namespace ContosoCrafts.WebSite.Models
         public string Image { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
-        public int[] Ratings { get; set; }
         public string DogType { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
